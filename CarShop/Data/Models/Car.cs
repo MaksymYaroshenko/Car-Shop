e06f164d03bc5f28e6feec29c0c9e7cce4769ca9
MaksymYaroshenko <53305575+MaksymYaroshenko@users.noreply.github.com>
2020-01-28 21:59:50 +0200

    Add models

---
+++ CarShop/Data/Models/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarShop.Data.Models
+{
+    public class Car
+    {
+        public int id { get; set; }
+
+        public string name { get; set; }
+
+        public string shortDescription { get; set; }
+
+        public string longDescription { get; set; }
+
+        public string image { get; set; }
+
+        public ushort price { get; set; }
+
+        public int available { get; set; }
+
+        public int categoryID { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}
