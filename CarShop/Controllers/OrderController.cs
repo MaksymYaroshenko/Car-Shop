1226dda37f4e41c293b367f638a1c18134e375bb
MaksymYaroshenko <53305575+MaksymYaroshenko@users.noreply.github.com>
2020-02-10 18:08:30 +0200

    Add an option of adding the item to cart

---
+++ CarShop/Controllers/OrderController.cs
@@ -0,0 +1,23 @@
+﻿using CarShop.Data.Interfaces;
+using CarShop.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarShop.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IAllOrders allOrders;
+        private readonly ShopCart shopCart;
+
+        public OrderController(IAllOrders allOrders, ShopCart shopCart)
+        {
+            this.allOrders = allOrders;
+            this.shopCart = shopCart;
+        }
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+    }
+}
