654464dce54388ce24d602ce37057f7b25c1e50b
MaksymYaroshenko <53305575+MaksymYaroshenko@users.noreply.github.com>
2020-03-27 22:10:32 +0200

    Add Order Controller

--- CarShop/Controllers/OrderController.cs
+++ CarShop/Controllers/OrderController.cs
@@ -19,5 +19,30 @@ namespace CarShop.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Chekout(Order order)
+        {
+            shopCart.ListShopItems = shopCart.GetShopItems();
+
+            if (shopCart.ListShopItems.Count == 0)
+            {
+                ModelState.AddModelError(string.Empty, "The Cart is empty");
+            }
+
+            if (ModelState.IsValid)
+            {
+                allOrders.CreateOrder(order);
+                return RedirectToAction("Complete");
+            }
+
+            return View();
+        }
+
+        public IActionResult Complete()
+        {
+            ViewBag.Message = "The Car is ordered";
+            return View();
+        }
     }
 }
